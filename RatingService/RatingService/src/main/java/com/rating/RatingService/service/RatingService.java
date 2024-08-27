8ee05f2442812c3e9c92fe439f614e938bbaf3f0
as090202 <as@extremenetworks.com>
2024-08-27 16:27:16 +0530

    Add files via upload

---
+++ RatingService/RatingService/src/main/java/com/rating/RatingService/service/RatingService.java
@@ -0,0 +1,15 @@
+package com.rating.RatingService.service;
+
+import com.rating.RatingService.entities.Rating;
+
+import java.util.List;
+
+public interface RatingService {
+    Rating create(Rating rating);
+
+    List<Rating> getRatings();
+
+    List<Rating> getRatingByUserId(String userId);
+
+    List<Rating> getRatingByHotelId(String hotelId);
+}
